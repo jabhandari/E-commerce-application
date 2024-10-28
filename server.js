3ccea75445d9b63671b04790be343c59ff6be803
jabhandari <jabhandari@myseneca.ca>
2024-10-27 20:44:14 -0400

    assignment3 part-2

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const express = require('express');
 const storeService = require("./store-service");
 const path = require("path");
 const app = express();
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -51,6 +55,10 @@ app.get('/categories', (req,res)=>{
     });
 });
 
+app.get('/items/add',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views','addItem.html'));
+});
+
 app.use((req,res)=>{
     res.status(404).send("404 - Page Not Found")
 })
@@ -62,3 +70,49 @@ storeService.initialize().then(()=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+
+cloudinary.config({
+    cloud_name: 'dyfd3rjkn',
+    api_key: '499268822322956',
+    api_secret: '-Lt12gltJ1e63Dp6GmbGtnDdJGw',
+    secure: true
+});
+
+const upload = multer(); // no { storage: storage } 
+
+app.post('/items/add', upload.single("featureImage"), (req, res) => {
+    if (req.file) {
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream((error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                });
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+
+        async function upload(req) {
+            let result = await streamUpload(req);
+            return result;
+        }
+
+        upload(req).then((uploaded) => {
+            processItem(uploaded.url);
+        });
+    } else {
+        processItem("");
+    }
+
+    function processItem(imageUrl) {
+        req.body.featureImage = imageUrl;
+
+        storeService.addItem(req.body)
+            .then(() => res.redirect('/items'))
+            .catch(err => res.status(500).send(err));
+    }
+});
--- store-service.js
+++ store-service.js
@@ -25,19 +25,29 @@ module.exports.initialize = function () {
 }
 
 module.exports.getAllItems = function(){
-    return new Promise((resolve,reject)=>{
-        (posts.length > 0 ) ? resolve(posts) : reject("no results returned"); 
+    return new Promise((resolve, reject) => {
+        (posts.length > 0) ? resolve(posts) : reject("no results returned"); 
     });
 }
 
 module.exports.getPublishedItems = function(){
-    return new Promise((resolve,reject)=>{
-        (items.length > 0) ? resolve(items.filter(item => items.published)) : reject("no results returned");
+    return new Promise((resolve, reject) => {
+        const publishedItems = posts.filter(item => item.published);
+        (publishedItems.length > 0) ? resolve(publishedItems) : reject("no results returned");
     });
 }
 
 module.exports.getCategories = function(){
-    return new Promise((resolve,reject)=>{
-        (categories.length > 0 ) ? resolve(categories) : reject("no results returned"); 
+    return new Promise((resolve, reject) => {
+        (categories.length > 0) ? resolve(categories) : reject("no results returned"); 
+    });
+}
+
+module.exports.addItem = function(itemData) {
+    return new Promise((resolve, reject) => {
+        itemData.published = itemData.published ? true : false;
+        itemData.id = posts.length + 1;
+        posts.push(itemData);
+        resolve(itemData);
     });
 }
